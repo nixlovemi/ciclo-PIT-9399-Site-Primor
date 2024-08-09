a7f2eb0990a1c4f2ffe19ad3f26cef3b9bd89785
Leandro Parra <nixlovemi@gmail.com>
2024-08-09 15:12:18 -0400

    fix: changed iFrame for video tag

--- resources/views/layout/video-embed.blade.php
+++ resources/views/layout/video-embed.blade.php
@@ -11,5 +11,10 @@ $roundedClass = ($ROUNDED) ? 'embed-responsive-rounded': '';
 @endphp
 
 <div class="embed-responsive embed-responsive-16by9 {{$roundedClass}} mb-5">
-    <iframe class="embed-responsive-item" src="{{ $VIDEO_SRC ?? '' }}" allowfullscreen></iframe>
+    <!--<iframe class="embed-responsive-item" src="{{ $VIDEO_SRC ?? '' }}" allowfullscreen></iframe>-->
+    
+    <video controls>
+        <source src="{{ $VIDEO_SRC ?? '' }}" />
+        Your browser does not support the video tag.
+    </video> 
 </div>
